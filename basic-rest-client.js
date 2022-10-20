e149085fb7aeea615865b7633b188607222427b5
Xavi Valdes <xavivaldes@gmail.com>
2022-10-20 19:46:13 +0200

    Lint and prettier

--- basic-rest-client.js
+++ basic-rest-client.js
@@ -1,60 +1,83 @@
 import fetch from 'node-fetch';
 
 class Service {
-  constructor({client, operation}) {
+  constructor({ client, operation }) {
     this.client = client;
     this.operation = operation;
   }
 
   r(id) {
-    return new Service({client: this.client, operation: `${this.operation}/${id}`});
+    return new Service({
+      client: this.client,
+      operation: `${this.operation}/${id}`,
+    });
   }
 
   async retrieve() {
-    return this.client.performCall({operation: this.operation});
+    return this.client.performCall({ operation: this.operation });
   }
 
   async create(data) {
-    return this.client.performCall({operation: this.operation, method: 'POST', body: data});
+    return this.client.performCall({
+      operation: this.operation,
+      method: 'POST',
+      body: data,
+    });
   }
 
   async update(data) {
-    return this.client.performCall({operation: this.operation, method: 'PATCH', body: data});
+    return this.client.performCall({
+      operation: this.operation,
+      method: 'PATCH',
+      body: data,
+    });
   }
 
   async replace(data) {
-    return this.client.performCall({operation: this.operation, method: 'PUT', body: data});
+    return this.client.performCall({
+      operation: this.operation,
+      method: 'PUT',
+      body: data,
+    });
   }
 
   async delete() {
-    return this.client.performCall({operation: this.operation, method: 'DELETE'});
+    return this.client.performCall({
+      operation: this.operation,
+      method: 'DELETE',
+    });
   }
 }
 
 export class Client {
-  constructor({endpoint, headers}) {
+  constructor({ endpoint, headers }) {
     this.endpoint = endpoint;
     this.headers = headers;
   }
 
-  performCall({operation = '', method = 'GET', body}) {
+  performCall({ operation = '', method = 'GET', body }) {
     const url = `${this.endpoint}/${operation}`;
     return fetch(url, {
       headers: {
         ...this.headers,
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       method,
-      body: body ? JSON.stringify(body) : null
+      body: body ? JSON.stringify(body) : null,
     })
-      .then(r => r.json())
-      .then(json => {
-        if (json.error) throw new Error(`Error calling ${method} ${url} body: ${JSON.stringify(body)}; Error: ${JSON.stringify(json.error)};`);
+      .then((r) => r.json())
+      .then((json) => {
+        if (json.error)
+          throw new Error(
+            `Error calling ${method} ${url} body: ${JSON.stringify(
+              body
+            )}; Error: ${JSON.stringify(json.error)};`
+          );
         return json;
       });
   }
 
   service(serviceName = '') {
-    return new Service({client: this, operation: serviceName});
+    return new Service({ client: this, operation: serviceName });
   }
 }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {Client} from './basic-rest-client.js';
+import { Client } from './basic-rest-client.js';
 
 const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
 const AIRTABLE_ENDPOINT = 'https://api.airtable.com';
@@ -15,7 +15,7 @@ class Table {
   }
 
   async getField(fieldId) {
-    return this.schema.fields.find(f => f.id === fieldId);
+    return this.schema.fields.find((f) => f.id === fieldId);
   }
 
   async createField(field) {
@@ -30,7 +30,7 @@ class Table {
 
   async renameField(fieldId, newName) {
     const service = this.service.r('fields').r(fieldId);
-    return service.update({name: newName});
+    return service.update({ name: newName });
   }
 }
 
@@ -45,8 +45,12 @@ class Base {
   async retrieveTables() {
     if (this.tables.size > 0) return;
     const service = this.service.r('tables');
-    this.tables = await service.retrieve().then(ss => ss.tables.map(s => new Table(s, service.r(s.id))));
-    this.tablesMap = new Map(this.tables.reduce((a, t) => [...a, [t.schema.id, t]], []));
+    this.tables = await service
+      .retrieve()
+      .then((ss) => ss.tables.map((s) => new Table(s, service.r(s.id))));
+    this.tablesMap = new Map(
+      this.tables.reduce((a, t) => [...a, [t.schema.id, t]], [])
+    );
   }
 
   async listTables() {
@@ -59,33 +63,45 @@ class Base {
     return this.tablesMap.get(tableId);
   }
 
-  async createTable({name, fields = [{type: "singleLineText", name: "name"}]}) {
-    return new Table(await performCall({
-      operation: `bases/${this.schema.id}/tables`,
-      method: 'POST',
-      body: {name, fields}
-    }), this.schema.id);
+  async createTable({
+    name,
+    fields = [{ type: 'singleLineText', name: 'name' }],
+  }) {
+    return new Table(
+      await performCall({
+        operation: `bases/${this.schema.id}/tables`,
+        method: 'POST',
+        body: { name, fields },
+      }),
+      this.schema.id
+    );
   }
 }
 
 export class AirtableMetadata {
-  constructor({endpoint = AIRTABLE_ENDPOINT, service = AIRTABLE_SERVICE_NAME, apiKey = AIRTABLE_API_KEY} = {}) {
+  constructor({
+    endpoint = AIRTABLE_ENDPOINT,
+    service = AIRTABLE_SERVICE_NAME,
+    apiKey = AIRTABLE_API_KEY,
+  } = {}) {
     this.service = new Client({
       endpoint,
       headers: {
-        'Authorization': `Bearer ${apiKey}`
-      }
+        Authorization: `Bearer ${apiKey}`,
+      },
     }).service(service);
   }
 
   async listBases() {
     const service = this.service.r('bases');
-    return baseService.retrieve().then(ss => ss.bases.map(s => new Base(s, service.r(s.id))));
+    return baseService
+      .retrieve()
+      .then((ss) => ss.bases.map((s) => new Base(s, service.r(s.id))));
   }
 
   async getBase(baseId) {
     const service = this.service.r('bases').r(baseId);
-    return service.retrieve().then(s => new Base(s, service));
+    return service.retrieve().then((s) => new Base(s, service));
   }
 }
 
